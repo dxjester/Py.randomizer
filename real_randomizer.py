9369b37f3eb7b716c5cf009dcb41c7a282daebc3
dxjester <patbenitez@gmail.com>
2019-07-19 09:48:55 -0400

    Create real_randomizer.py

---
+++ real_randomizer.py
@@ -0,0 +1,19 @@
+# FILENAME: real_randomizer.py
+# PROJECT: Randomizer Project
+# DATE CREATED: 19-July-19
+# DATE MODIFIED: 19-July-19
+# VERSION: 1.0
+# COMMENTS: A Python script designed 
+
+# generate random floating point values
+import numpy as np
+
+# generate random floating point values
+from numpy.random import seed
+from numpy.random import randint
+
+seed(6)
+
+size = int(input("Enter a number: "))
+values = randint(0, 10, size)
+print(values)
