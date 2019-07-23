083270911e8439e706962c273d9c8cf46cf7a579
dxjester <patbenitez@gmail.com>
2019-07-23 22:42:17 +0000

    Update real_randomizer.py

--- real_randomizer.py
+++ real_randomizer.py
@@ -8,13 +8,19 @@
 # generate random floating point values
 import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import random
 import datetime
 
 # generate random floating point values
 from numpy.random import seed
 from numpy.random import randint
 
+#-------------------------- PHASE 1 -----------------------------#
+#---------------------- Class Definition ------------------------#
+#----------------------------------------------------------------#
+
+# 1.1 Declare the class -----------------------------------------#
 seed(6)
 
 
@@ -24,7 +30,7 @@ class int_array:
         self.master_df = pd.DataFrame(index = np.arange(self.size), columns=['timesheet_date','timestamp','hours'])
         self.master_df['timesheet_date'] = datetime.datetime.now()
         self.master_df['timestamp'] = datetime.datetime.now()
-        self.master_df['hours'] = np.random.choice([1, 9, 20], self.master_df.shape[0])
+        self.master_df['hours'] = np.random.randint(0,12,size=(self.size, 1))
         self.mean = 0
         self.standard_dev = 0
         self.variance = 0
@@ -35,15 +41,16 @@ class int_array:
         self.standard_dev = self.master_df['hours'].std()
         self.variance = self.master_df['hours'].var()
     def hist_plot(self):
-        plt.hist(self.values, normed=True)
+        plt.hist(self.master_df['hours'], normed=True)
     def print_array(self):
         print(self.master_df)
         print("The mean of the array is ", self.mean)
         print("The variance of the array is ", self.variance)
         print("The standard deviation of the array is ", self.standard_dev)
 
+# 2.1 Script Execution Start -----------------------------------------#
 initial_array = int_array()
 initial_array.calculate()        
 initial_array.print_array()
 
-#initial_array.hist_plot()
+
